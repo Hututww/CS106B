fc90a23e6c1c2c5ee8762db0882a067f8cca9872
饭斗花园土土大王 <stevechen2005@outlook.com>
2025-03-22 18:09:39 +0800

    Delete Assignment 9 directory

--- Assignment 9/Demos/GUIConfig.h
+++
@@ -1,8 +0,0 @@
-RUN_TESTS_MENU_OPTION()
-
-MENU_ORDER("CompressGUI.cpp", "DecompressGUI.cpp")
-
-WINDOW_TITLE("Huffman Coding")
-
-TEST_BARRIER("CompressGUI.cpp", "Huffman.cpp")
-TEST_BARRIER("DecompressGUI.cpp", "Huffman.cpp")
